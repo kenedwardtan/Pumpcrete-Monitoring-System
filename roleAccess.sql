52f599b1b5eb2063a3c689098414da283f90bccf
amndaprz <amanda_perez@dlsu.edu.ph>
2021-08-18 20:45:25 +0800

    Fixed authorized access
    Please run roleAccess.sql  after restoring the pumpcrete.

--- roleAccess.sql
+++ roleAccess.sql
@@ -1,3 +1,6 @@
+GRANT UPDATE on "users" to admin_role; --admin can update
+GRANT INSERT on "users" to admin_role; --admin can create new
+GRANT SELECT on "users" to admin_role; --admin can view users
 
 --CLIENTS
 GRANT UPDATE on "client" to staff_role;
@@ -19,4 +22,26 @@ GRANT UPDATE on "credit_memo" to staff_role;
 GRANT INSERT on "credit_memo" to staff_role; 
 GRANT SELECT on "credit_memo" to staff_role; 
 GRANT DELETE on "credit_memo" to staff_role; 
+--PSC
+GRANT UPDATE on "psc" to staff_role;
+GRANT INSERT on "psc" to staff_role; 
+GRANT SELECT on "psc" to staff_role; 
+GRANT DELETE on "psc" to staff_role; 
+
+--UPDATE AND VIEW PASSWORD
+GRANT UPDATE on "users" to supervisor_role;
+GRANT SELECT on "users" to supervisor_role;
+--CLIENTS
+GRANT UPDATE on "client" to supervisor_role;
+GRANT SELECT on "client" to supervisor_role; 
+--BILLINGS
+GRANT UPDATE on "billings" to supervisor_role;
+GRANT SELECT on "billings" to supervisor_role; 
+--PUMPCRETE
+GRANT UPDATE on "pumpcretes" to supervisor_role;
+GRANT SELECT on "pumpcretes" to supervisor_role; 
+--CREDIT MEMOS
+GRANT UPDATE on "credit_memo" to supervisor_role;
+GRANT SELECT on "credit_memo" to supervisor_role; 
+
 
