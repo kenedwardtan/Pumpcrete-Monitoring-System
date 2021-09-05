73b6328c4f5d43f286f2d31dd2a998aa491c5973
TanTheFatman <74177343+TanTheFatman@users.noreply.github.com>
2021-09-05 17:51:46 +0800

    updated clients table, fixed landline example

---
+++ clientsUpdate.sql
@@ -0,0 +1,21 @@
+
+DROP TABLE IF EXISTS client CASCADE;
+
+CREATE TABLE client (
+	 client_id BIGSERIAL NOT NULL PRIMARY KEY,
+	 client_first_name VARCHAR NOT NULL,
+	 client_last_name VARCHAR NOT NULL,
+	 client_position VARCHAR NOT NULL,
+	 client_address VARCHAR NOT NULL,
+	 client_landline VARCHAR NOT NULL,
+	 client_cellphone VARCHAR NOT NULL,
+	 client_email VARCHAR NOT NULL,
+	 latest_doc_date DATE);
+
+
+ -- Modify initial value and increment
+   ALTER SEQUENCE client_client_id_seq RESTART WITH 1 INCREMENT BY 1;
+ 
+		
+			
+			
